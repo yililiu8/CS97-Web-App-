0be62b61dc3451739a87f1abd3c93d01fa3c694a
Jonathan Lam <jlam7@g.ucla.edu>
2020-11-10 23:22:17 -0500

    Clickable Text

--- assignment_list1/src/index.js
+++ assignment_list1/src/index.js
@@ -3,29 +3,61 @@ import ReactDOM from 'react-dom';
 import './index.css';
   
 class Textbox extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            clicked: false,
+        };
+    }
+    renderButton(txt){
+        return (
+            <Redirect 
+                text={txt}
+                onClick={()=>this.onClick()}
+            />
+        )
+    }
+    onClick(){
+        this.setState({
+            clicked: !this.state.clicked
+        });
+        console.log("here");
+    }
     render (){
         return (
         <div className="txt-box">
-            <div>{"RandomTest"}</div>
+            <div>{"Upcoming Assignments"}</div>
             <div className="assignments">
             	<ul>
-                    <li>{"Assignment 1"}
+                    <p>{this.renderButton("Assignment 1")}</p>
                         <ul>
                             <li>{"due date 11/7"}</li>
                             <li>{"at 7:00pm PST"}</li>
+                            <li>{"submission link"}</li>
                         </ul>
-                    </li>
-                    <li>{"Assignment 2"}
+                    <p>{this.renderButton("Assignment 2")}</p>
                         <ul>
                             <li>{"due date 11/8"}</li>
                             <li>{"at 8:00pm PST"}</li>
+                            <li>{"submission link"}</li>
                         </ul>
-                    </li>
+                        <div>
+                            {'STATE OF THE BUTTON'}
+                            <div>{this.state.clicked ? "CLICKED" : "NOT CLICKED"}</div>
+                        </div>
                 </ul>
             </div>
         </div>
         );
     }
+}
+
+function Redirect(props) {
+    return (
+        <button className="redirect" onClick={props.onClick}> 
+            {props.text}
+        </button>
+    );
 }
 
 ReactDOM.render(
