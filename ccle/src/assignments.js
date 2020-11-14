a6d4c40419afc02bcdf2e1e96aa38d81be09e66b
Jonathan Lam <jlam7@g.ucla.edu>
2020-11-13 23:26:05 -0500

    Added ability for buttons to act independently

--- ccle/src/assignments.js
+++ ccle/src/assignments.js
@@ -6,22 +6,31 @@ export class UpcomingAssignments extends React.Component {
     constructor(props){
         super(props);
         this.state={
-            clicked: false,
-        };
+            assignment_list: ["Class 1", "Class 2"], // Create a function that can retrieve
+            button_status: [false, false], // necessary number of classes and names
+            class_index : { // also create dictionary to be used
+                "Assignment 1" : 0,
+                "Assignment 2" : 1
+                }
+            };
     }
+
     renderButton(txt){
         return (
-            <Redirect 
+            <Assignment 
                 text={txt}
-                onClick={()=>this.onClick()}
+                onClick={()=>this.onClick(txt)}
             />
         )
     }
-    onClick(){
+    onClick(txt){
+        const map_index=this.state.class_index[txt];
+        const a = this.state.button_status.slice();
+        a[map_index] = !a[map_index];
         this.setState({
-            clicked: !this.state.clicked
+            button_status: a,
         });
-        console.log("here");
+        console.log(a);
     }
     render() {
       return (
@@ -41,10 +50,6 @@ export class UpcomingAssignments extends React.Component {
                             <li>{"at 8:00pm PST"}</li>
                             <li>{"submission link"}</li>
                         </ul>
-                        <div>
-                            {'STATE OF THE BUTTON:'}
-                            <div>{this.state.clicked ? "CLICKED" : "NOT CLICKED"}</div>
-                        </div>
                 </ul>
             </div>
         </div>
@@ -52,12 +57,26 @@ export class UpcomingAssignments extends React.Component {
     }
 }
 
-
-
-function Redirect(props) {
-    return (
-        <button className="redirect" onClick={props.onClick}> 
-            {props.text}
-        </button>
-    );
+class Assignment extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            clicked: false,
+        };
+    }
+    render () {
+        return (
+            <button className="redirect" onClick={this.props.onClick}>
+                {this.props.text}
+            </button>
+            );
+    }
 }
+
+// function Redirect(props) {
+//     return (
+//         <button className="redirect" onClick={props.onClick}> 
+//             {props.text}
+//         </button>
+//     );
+// }
